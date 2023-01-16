502733ebca29b1ba5217265aa98c6d5555309ffe
Mohamed Belkamel <belkamelmohamed@gmail.com>
2023-01-16 14:28:43 +0100

    feat(terminal): added copy button and styling

--- src/app/multichoicecomponent/terminal/terminal.component.ts
+++ src/app/multichoicecomponent/terminal/terminal.component.ts
@@ -1,17 +1,38 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'terminal',
   templateUrl: './terminal.component.html',
   styleUrls: ['./terminal.component.css'],
 })
-export class TerminalComponent {
+export class TerminalComponent implements OnInit {
   @Input() prompt: string = '$';
+  @Input() type: 'input' | 'output' = 'input';
   public currentCommand!: string;
   public commands: string[] = [];
 
   runCommand() {
     this.commands.push(this.currentCommand);
     this.currentCommand = '';
+  }
+
+  copyCommand() {
+    const lastCommand = this.prompt;
+    const selBox = document.createElement('textarea');
+    selBox.style.position = 'fixed';
+    selBox.style.left = '0';
+    selBox.style.top = '0';
+    selBox.style.opacity = '0';
+    selBox.value = lastCommand.replace('$ ', '');
+    document.body.appendChild(selBox);
+    selBox.focus();
+    selBox.select();
+    document.execCommand('copy');
+    document.body.removeChild(selBox);
+  }
+
+  ngOnInit(): void {
+    this.prompt =
+      this.type === 'input' ? `$ ${this.prompt}` : `> ${this.prompt}`;
   }
 }
